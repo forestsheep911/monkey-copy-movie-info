cd44149c0818294093a985962163a78f9beb441d
徐冰 <forestsheep911@gmail.com>
2024-08-15 17:22:17 +0800

    first save

--- config/dev.meta.js
+++ config/dev.meta.js
@@ -1,9 +1,9 @@
 const pj = require('../package.json')
 const { resolve } = require('path')
 module.exports = {
-  name: `${pj.name}`,
-  namespace: `https://github.com/${pj.author}/monkey-${pj.name}`,
-  match: ['https://*'],
+  name: pj.name,
+  namespace: pj.name,
+  match: ['https://movie.douban.com/subject/*', 'http://localhost:8080'],
   grant: ['GM_getValue', 'GM_setValue', 'GM_addValueChangeListener'],
   require: [`file://${resolve(__dirname, '../dist/dev').replaceAll('\\', '/')}/${pj.name}.script.js`],
 }
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,42 @@
 const app = () => {
-  console.log('monkey jumping on the bed.')
+  console.log('monkey jumping on the bed1.')
+  const doubanInfo = document.querySelector('#info')
+  if (doubanInfo) {
+    console.log(doubanInfo)
+  } else {
+    console.log('no douban info')
+  }
+  // find element in doubanInfo which tag is span and style is display: none
+  const disabledInfo = doubanInfo?.querySelectorAll('span[style*="display: none"]')
+  if (disabledInfo) {
+    console.log(disabledInfo)
+  } else {
+    console.log('no disabled info')
+  }
+  // make disabledInfo visible (inline)
+  if (disabledInfo?.length) {
+    disabledInfo.forEach((item) => {
+      item.setAttribute('style', 'display: inline')
+    })
+  }
+  // remove <a href="javascript:;" class="more-attrs" title="显示更多">更多...</a>
+  const moreAttrs = doubanInfo?.querySelector('.more-attrs')
+  if (moreAttrs) {
+    moreAttrs.remove()
+  }
+  // copy info into clipboard
+  const info = doubanInfo?.textContent
+  if (info) {
+    console.log(info.trim())
+  }
+  // create a copy button to copy info
+  const copyButton = document.createElement('button')
+  copyButton.textContent = 'Copy'
+  copyButton.setAttribute('style', 'position: fixed; top: 0; right: 0; z-index: 9999;')
+  copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(info?.trim() || '')
+  })
+  doubanInfo?.appendChild(copyButton)
 }
 
 export default app
